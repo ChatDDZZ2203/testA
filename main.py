3ab98fcec93cce9de2c5c3d6973f163c3d089567
ChatDDZZ2203 <dandy.d22.z03@gmail.com>
2023-07-16 13:20:12 +0300

    Add selenium to requirements.txt

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from flask import Flask, request
 app = Flask(__name__)
 db = Database(os.environ['REPLIT_DB_URL'])
 
+
 @app.route('/', methods=['GET'])
 def handle_request():
     return f"Hi from adaptable\n\n{db['response']}"
+
 
 app.run('0.0.0.0', os.getenv('PORT', 3000))
 

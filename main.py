22ab5764e0aa3cf19c910549b82656c517da20bc
ChatDDZZ2203 <125794957+ChatDDZZ2203@users.noreply.github.com>
2023-07-16 08:22:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 
+from replit import Database
 from flask import Flask, request
 
 app = Flask(__name__)
+db = Database(os.environ['REPLIT_DB_URL'])
 
 @app.route('/', methods=['GET'])
 def handle_request():
-    return "Hi from adaptable"
+    return f"Hi from adaptable\n\n{db['response']}"
 
 app.run('0.0.0.0', os.getenv('PORT', 3000))
 
